31861188b067a9c63e0ad27107c5c8711f6aabaf
alessiogiovagnini <alessio.giovagnini@usi.ch>
2025-01-01 17:04:17 +0100

    exercise 5 done

--- src-profiler/ex5/Instrumentation.java
+++ src-profiler/ex5/Instrumentation.java
@@ -1,5 +1,19 @@
 package ex5;
 
+import ch.usi.dag.disl.marker.BodyMarker;
+import ch.usi.dag.disl.annotation.AfterReturning;
+import ch.usi.dag.disl.dynamiccontext.DynamicContext;
+
+import java.util.Arrays;
+
 public class Instrumentation {
+
+    @AfterReturning(marker = BodyMarker.class, scope="ex5.MainThread.foo")
+    static void afterMethodFoo(DynamicContext dc) {
+        int input = dc.getMethodArgumentValue(0, int.class);
+        int[] out = (int[]) dc.getStackValue(0, Object.class);
+        System.out.println("Input: " + input + " - Output: " + Arrays.toString(out));
+        // is the Fibonacci sequence
+    }
 
 }
