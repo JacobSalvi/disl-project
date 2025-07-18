6d6f5de8884952bbedb28cb39067e70d08cd1078
alessiogiovagnini <alessio.giovagnini@usi.ch>
2025-07-18 08:14:44 +0200

    edited the scripts to start the instrumentation

--- runInstrumented.sh
+++ runInstrumented.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 
 if [ "$#" -ne 1 ]; then
@@ -9,24 +9,27 @@ fi
 AGENT_EXT=.so
 
 #Checks if OS is MacOS
-if [ `uname -s` == "Darwin" ]; then
-    AGENT_EXT=.jnilib
-fi
+#if [ `uname -s` == "Darwin" ]; then
+#    AGENT_EXT=.jnilib
+#    echo "you shouldn't see this!!!!!!!!"
+#fi
 
-ARCH=x86_64
+# ask if disl compile by default for the system that compile it
+#ARCH=x86_64
 
 #Check if architecture is aarch64
 
-if [ `uname -p` = "aarch64" ]; then
-    ARCH=aarch64
-fi
+#if [ `uname -p` = "aarch64" ]; then
+#    ARCH=aarch64
+#fi
 
-AGENT_FLAGS=
+#AGENT_FLAGS=
 
-JAVA_VERSION=$("$JAVA_HOME/bin/java" -version 2>&1 | head -1 | cut -d'"' -f2 | sed '/^1\./s///' | cut -d'.' -f1)
+#JAVA_VERSION=$("$JAVA_HOME/bin/java" -version 2>&1 | head -1 | cut -d'"' -f2 | sed '/^1\./s///' | cut -d'.' -f1)
 
-if [ "$JAVA_VERSION" -gt "8" ]; then
-  AGENT_FLAGS="$AGENT_FLAGS --patch-module java.base=lib/disl-bypass.jar --add-exports java.base/ch.usi.dag.disl.dynamicbypass=ALL-UNNAMED"
-fi
 
-$JAVA_HOME/bin/java -agentpath:lib/$ARCH/libdislagent$AGENT_EXT $AGENT_FLAGS -Xbootclasspath/a:lib/disl-bypass.jar:build/profiler.jar -cp build/app.jar -noverify $*
+AGENT_FLAGS="$AGENT_FLAGS --patch-module java.base=lib/disl-bypass.jar --add-exports java.base/ch.usi.dag.disl.dynamicbypass=ALL-UNNAMED"
+echo "agent flags"
+echo "$AGENT_FLAGS"
+
+$JAVA_HOME/bin/java --enable-preview -agentpath:lib/libdislagent$AGENT_EXT $AGENT_FLAGS -Xbootclasspath/a:lib/disl-bypass.jar:build/profiler.jar -cp build/app.jar -noverify $1
--- startDiSLServer.sh
+++ startDiSLServer.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-$JAVA_HOME/bin/java -Ddisl.exclusionList="exclusion.lst" -cp build/profiler.jar:lib/disl-server.jar ch.usi.dag.dislserver.DiSLServer &
+$JAVA_HOME/bin/java --enable-preview -Ddisl.exclusionList="exclusion.lst" -cp build/profiler.jar:lib/disl-server.jar ch.usi.dag.dislserver.DiSLServer &
