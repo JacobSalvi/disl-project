c15b09e26b193f098fbcd7f2b3f9957c3ee25551
alessiogiovagnini <alessio.giovagnini@usi.ch>
2025-07-23 09:40:46 +0200

    small change

--- disl.sh
+++ disl.sh
@@ -8,12 +8,12 @@ fi
 ant clean
 echo "> cleaned"
 
-start_time=$(($(date +%s%N)))
 ant -Ddislclass=ex"$1".Instrumentation
 ./startDiSLServer.sh
 sleep 2
 echo "> server started"
 echo "> running ex $1"
+start_time=$(($(date +%s%N)))
 ./runInstrumented.sh ex"$1".Main
 end_time=$(($(date +%s%N)))
 elapsed="$(($end_time-$start_time))"
